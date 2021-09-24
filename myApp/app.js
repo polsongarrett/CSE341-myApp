34504a1003e4024883ec6997fa03fcb83adfa110
polsongarrett <garrettpolson94@gmail.com>
2021-09-24 17:00:11 -0600

    first commit

---
+++ myApp/app.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const http = require('http'); // imported for creating a server
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+// const expressHbs = require('express-handlebars');
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+
+app.set('view engine', 'ejs'); // change based on engine: pug, hbs, ejs
+app.set('views', 'views'); // default where to find templates
+
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin',adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', {docTitle: 'Page Not Found', path: ''});
+});
+
+app.listen(3000);
---
+++ myApp/routes/admin.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path')
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    docTitle: 'Add Product', 
+    path: '/admin/add-product',
+    activeAddProduct: true,
+    productCSS: true
+  });
+});
+
+router.post('/add-product', (req, res, next) => {
+  products.push(
+    {
+      title: req.body.title, 
+      price: req.body.price, 
+      description: req.body.description
+    });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
